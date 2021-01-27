51eb61127e1dfd4cfde0393c918126a4dbc6e8f5
AngryCouchPotato <alexeykolcov@gmail.com>
2021-01-27 22:18:17 +0100

    solve 1 medium task:        24. Swap Nodes in Pairs

---
+++ src/com/edu/leetcoding/linkedlist/SwapNodesInPairs.java
@@ -0,0 +1,37 @@
+package com.edu.leetcoding.linkedlist;
+
+/*
+    24. Swap Nodes in Pairs
+
+    Given a linked list, swap every two adjacent nodes and return its head.
+
+    Example 1:
+        Input: head = [1,2,3,4]
+        Output: [2,1,4,3]
+    Example 2:
+        Input: head = []
+        Output: []
+    Example 3:
+        Input: head = [1]
+        Output: [1]
+    Constraints:
+        The number of nodes in the list is in the range [0, 100].
+        0 <= Node.val <= 100
+
+    Follow up: Can you solve the problem without modifying the values in the list's nodes? (i.e., Only nodes themselves may be changed.)
+
+ */
+public class SwapNodesInPairs {
+
+    // O(n) time : O(n) space, where n - length of the list
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = head.next;
+        head.next = temp.next;
+        temp.next = head;
+        head.next = swapPairs(head.next);
+        return temp;
+    }
+}
