b3b3df182f95a54d6d0661504317becb149a5ea6
AngryCouchPotato <alexeykolcov@gmail.com>
2021-01-30 23:48:42 +0100

    solve 1 easy task:        1299. Replace Elements with Greatest Element on Right Side

---
+++ src/com/edu/leetcoding/array/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,46 @@
+package com.edu.leetcoding.array;
+
+/*
+    1299. Replace Elements with Greatest Element on Right Side
+
+    Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+    After doing so, return the array.
+
+    Example 1:
+        Input: arr = [17,18,5,4,6,1]
+        Output: [18,6,6,6,1,-1]
+        Explanation:
+        - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+        - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+        - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+        - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+        - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+        - index 5 --> there are no elements to the right of index 5, so we put -1.
+    Example 2:
+        Input: arr = [400]
+        Output: [-1]
+        Explanation: There are no elements to the right of index 0.
+    Constraints:
+        1 <= arr.length <= 104
+        1 <= arr[i] <= 105
+
+ */
+public class ReplaceElementsWithGreatestElementOnRightSide {
+
+    public int[] replaceElements(int[] arr) {
+        if (arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        }
+        int id = arr.length - 2;
+        int max = arr[arr.length - 1];
+        int temp;
+        while (id >= 0) {
+            temp = arr[id];
+            arr[id--] = max;
+            max = Math.max(max, temp);
+        }
+        arr[arr.length - 1] = -1;
+        return arr;
+    }
+}
