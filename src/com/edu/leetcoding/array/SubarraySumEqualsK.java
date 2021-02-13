4e2cd3cc7351b911b69a235711a0b92203c1a8ff
AngryCouchPotato <alexeykolcov@gmail.com>
2021-02-13 23:44:43 +0100

    solve 1 medium task:          560. Subarray Sum Equals K

---
+++ src/com/edu/leetcoding/array/SubarraySumEqualsK.java
@@ -0,0 +1,55 @@
+package com.edu.leetcoding.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  560. Subarray Sum Equals K
+ *
+ *  Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+ *
+ *  Example 1:
+ *      Input: nums = [1,1,1], k = 2
+ *      Output: 2
+ *  Example 2:
+ *      Input: nums = [1,2,3], k = 3
+ *      Output: 2
+ *  Constraints:
+ *      1 <= nums.length <= 2 * 104
+ *      -1000 <= nums[i] <= 1000
+ *      -107 <= k <= 107
+ *
+ * */
+public class SubarraySumEqualsK {
+
+    // O(n) - time | O(n) - space
+    public int subarraySum(int[] nums, int k) {
+        int totalCount = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                totalCount += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return totalCount;
+    }
+
+    // O(n^2) - time | O(1) - space
+    public int subarraySum2(int[] nums, int k) {
+        int totalCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    totalCount++;
+                }
+            }
+        }
+        return totalCount;
+    }
+}
