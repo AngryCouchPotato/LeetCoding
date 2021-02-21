55b5fd65c37b1e55d4ee501ce2aca20b446b37ba
AngryCouchPotato <alexeykolcov@gmail.com>
2021-02-21 22:37:01 +0100

    solve 1 easy task:        852. Peak Index in a Mountain Array

---
+++ src/com/edu/leetcoding/binarysearch/PeakIndexInAMountainArray.java
@@ -0,0 +1,54 @@
+package com.edu.leetcoding.binarysearch;
+
+/**
+ *  852. Peak Index in a Mountain Array
+ *
+ *  Let's call an array arr a mountain if the following properties hold:
+ *      arr.length >= 3
+ *      There exists some i with 0 < i < arr.length - 1 such that:
+ *      arr[0] < arr[1] < ... arr[i-1] < arr[i]
+ *      arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+ *  Given an integer array arr that is guaranteed to be a mountain, return any i such
+ *  that arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+ *
+ *  Example 1:
+ *      Input: arr = [0,1,0]
+ *      Output: 1
+ *  Example 2:
+ *      Input: arr = [0,2,1,0]
+ *      Output: 1
+ *  Example 3:
+ *      Input: arr = [0,10,5,2]
+ *      Output: 1
+ *  Example 4:
+ *      Input: arr = [3,4,5,1]
+ *      Output: 2
+ *  Example 5:
+ *      Input: arr = [24,69,100,99,79,78,67,36,26,19]
+ *      Output: 2
+ *  Constraints:
+ *      3 <= arr.length <= 104
+ *      0 <= arr[i] <= 106
+ *      arr is guaranteed to be a mountain array.
+ *  Follow up: Finding the O(n) is straightforward, could you find an O(log(n)) solution?
+ *
+ * */
+public class PeakIndexInAMountainArray {
+
+    /**
+     *  O(log(n)) - time | O(1) - space
+     */
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            int mid = (right + left) / 2;
+            if (arr[mid + 1] > arr[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
