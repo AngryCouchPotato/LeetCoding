ac5e8bb83dfbe52cb7ec79a8dd45915aa0f661db
AngryCouchPotato <alexeykolcov@gmail.com>
2021-03-02 22:42:40 +0100

    solve 1 easy task:        278. First Bad Version

---
+++ src/com/edu/leetcoding/binarysearch/FirstBadVersion.java
@@ -0,0 +1,57 @@
+package com.edu.leetcoding.binarysearch;
+
+/**
+ *  278. First Bad Version
+ *
+ *  You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version
+ *  of your product fails the quality check. Since each version is developed based on the previous version,
+ *  all the versions after a bad version are also bad.
+ *  Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the
+ *  following ones to be bad.
+ *
+ *  You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find
+ *  the first bad version. You should minimize the number of calls to the API.
+ *
+ *  Example 1:
+ *      Input: n = 5, bad = 4
+ *      Output: 4
+ *      Explanation:
+ *          call isBadVersion(3) -> false
+ *          call isBadVersion(5) -> true
+ *          call isBadVersion(4) -> true
+ *          Then 4 is the first bad version.
+ *  Example 2:
+ *      Input: n = 1, bad = 1
+ *      Output: 1
+ *  Constraints:
+ *      1 <= bad <= n <= 231 - 1
+ *
+ * */
+public class FirstBadVersion {
+
+    /**
+     *  O(log(n)) - time | O(1) - space
+     */
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            boolean isBad = isBadVersion(mid);
+            if (isBad) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
+    /**
+     *  The isBadVersion API is defined in the parent class VersionControl.
+     * */
+    private boolean isBadVersion(int version) {
+        // stub
+        return true;
+    }
+}
