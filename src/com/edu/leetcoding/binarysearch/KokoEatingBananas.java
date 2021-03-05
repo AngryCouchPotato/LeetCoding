469f222f57426d2c681c6af5fa8fbcee2a833d11
AngryCouchPotato <alexeykolcov@gmail.com>
2021-03-05 19:05:07 +0100

    solve 1 medium task:        875. Koko Eating Bananas

---
+++ src/com/edu/leetcoding/binarysearch/KokoEatingBananas.java
@@ -0,0 +1,63 @@
+package com.edu.leetcoding.binarysearch;
+
+/**
+ *  875. Koko Eating Bananas
+ *
+ *  Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas.
+ *  The guards have gone and will come back in h hours.
+ *
+ *  Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and
+ *  eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will
+ *  not eat any more bananas during this hour.
+ *
+ *  Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+ *
+ *  Return the minimum integer k such that she can eat all the bananas within h hours.
+ *
+ *  Example 1:
+ *      Input: piles = [3,6,7,11], h = 8
+ *      Output: 4
+ *  Example 2:
+ *      Input: piles = [30,11,23,4,20], h = 5
+ *      Output: 30
+ *  Example 3:
+ *      Input: piles = [30,11,23,4,20], h = 6
+ *      Output: 23
+ *  Constraints:
+ *      1 <= piles.length <= 104
+ *      piles.length <= h <= 109
+ *      1 <= piles[i] <= 109
+ *
+ * */
+public class KokoEatingBananas {
+
+    /**
+     *  O(n*log(n)) - time | O(1) - space
+     */
+    public int minEatingSpeed(int[] piles, int H) {
+        int left = 1;
+        int right = 1_000_000_000;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int hours = calculateHours(mid, piles);
+            if (hours <= H) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private int calculateHours(int eatingSpeed, int[] piles) {
+        int hours = 0;
+        for (int i = 0; i < piles.length; i++) {
+            int current = piles[i];
+            hours += current / eatingSpeed;
+            if (current % eatingSpeed > 0) {
+                hours += 1;
+            }
+        }
+        return hours;
+    }
+}
