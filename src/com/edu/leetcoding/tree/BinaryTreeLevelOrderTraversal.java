9584f800ef930a2e8bac35fcf08ba83eb044ebe5
AngryCouchPotato <alexeykolcov@gmail.com>
2021-07-16 19:04:43 +0200

    formatting

--- src/com/edu/leetcoding/tree/BinaryTreeLevelOrderTraversal.java
+++ src/com/edu/leetcoding/tree/BinaryTreeLevelOrderTraversal.java
@@ -49,22 +49,22 @@ public class BinaryTreeLevelOrderTraversal {
     //  O(n) time : O(n) space
     public List<List<Integer>> levelOrderBFS(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if(root == null){
+        if (root == null) {
             return result;
         }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int size = queue.size();
             List<Integer> list = new ArrayList<>(size);
             result.add(list);
-            while(size > 0) {
+            while (size > 0) {
                 TreeNode node = queue.poll();
                 list.add(node.val);
-                if(node.left != null) {
+                if (node.left != null) {
                     queue.add(node.left);
                 }
-                if(node.right != null) {
+                if (node.right != null) {
                     queue.add(node.right);
                 }
                 size--;
